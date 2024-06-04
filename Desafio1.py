9fb020759d9be0369f2cc9e349c70ede100d8e6a
Rosalinda Laass Groner <laassrosa@gmail.com>
2024-06-04 18:59:07 -0300

    feat: resolução do desafio 1 e 2.

--- Desafio1.py
+++ Desafio1.py
@@ -1,2 +1,3 @@
 #Crie um programa que imprima na tela "Olá mundo Python!" no console
 
+print ("Ola mundo Python!")
--- Desafio2.py
+++ Desafio2.py
@@ -2,3 +2,8 @@
 #nome como uma string, e outra chamada 'idade', que deve guardar a sua idade como um número inteiro. Depois 
 #disso, eu quero que você imprima na tela uma frase que diga "Olá, meu nome é [nome] e eu tenho [idade] anos". 
 #Lembre-se de substituir [nome] e [idade] pelos valores das variáveis que você declarou.
+
+nome = input("Seu nome: ")
+idade = int(input("Sua idade: "))
+
+print("Olá, meu nome é " + nome + " e eu tenho " + str(idade) + " anos")
